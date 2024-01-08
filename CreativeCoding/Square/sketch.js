dab28ac3087f1e743ae706f2aa876c2f1aed5327
abbyhall25 <abigailellahall@gmail.com>
2024-01-08 08:53:23 -0500

    Square sketch

--- CreativeCoding/Square/sketch.js
+++ CreativeCoding/Square/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
+  createCanvas (720,720);
+  background ("lightblue");
+  fill ("pink");
+  strokeWeight (8);
 
 }
 
 function draw() {
+  
+  translate (60,60);
+  quad (
+    0,0,
+    300,0,
+    300,300,
+    0,300
+
+  );
   
 }
