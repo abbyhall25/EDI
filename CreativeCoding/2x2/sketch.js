3c05e5e19692a2a264f80529f046933a2cb79632
abbyhall25 <abigailellahall@gmail.com>
2024-01-08 08:58:14 -0500

    2x2 Script

--- CreativeCoding/2x2/sketch.js
+++ CreativeCoding/2x2/sketch.js
@@ -1,7 +1,43 @@
 function setup() {
+  createCanvas (720,720);
+  background ("lightblue");
+  fill ("pink");
+  strokeWeight (8);
 
 }
 
 function draw() {
+  
+  translate (60,60);
+  quad (
+    0,0,
+    300,0,
+    300,300,
+    0,300
+
+  );
+  
+   quad (
+    300,0,
+    600,0,
+    600,300,
+    300,300
+
+  );
+  quad (
+    0,300,
+    300,300,
+    300,600,
+    0,600
+
+  );
+  
+   quad (
+    300,300,
+    600,300,
+    600,600,
+    300,600
+
+  );
   
 }
