4bef740da4af889e1ef579d0f762fcbfb7884515
abbyhall25 <abigailellahall@gmail.com>
2024-01-09 11:45:22 -0500

    Tuning with variables

--- CreativeCoding/TuningWithVariables/sketch.js
+++ CreativeCoding/TuningWithVariables/sketch.js
@@ -1,7 +1,28 @@
 function setup() {
+  createCanvas (windowWidth,windowHeight);
+  fill ("lightblue");
+  strokeWeight (1);
 
 }
 
 function draw() {
+  var num = 25;
+  var sideLen = windowWidth/num;
+ 
   
+  for (var y=0; y < windowHeight; y = y+sideLen) {
+    for (var x=0; x < windowWidth; x=x+sideLen){
+      quad (
+        x,y,
+        x+sideLen,y,
+        x+sideLen,y+sideLen,
+        x,y+sideLen
+      );
+          }
+}
+
+  
+}
+function windowResize (){
+  resizeCanvas (windowWidth, windowHeight);
 }
