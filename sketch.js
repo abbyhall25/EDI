c67afad30b6d6485331d0d6f950d738130372069
abbyhall25 <abigailellahall@gmail.com>
2024-01-24 11:31:08 -0500

    Update

---
+++ sketch.js
@@ -0,0 +1,34 @@
+//adding a Gif to my grid
+
+let gif;
+
+function preload (){
+	gif = loadImage('Assets/AbbyFinalGIF.gif');
+	}
+
+function setup() {
+  createCanvas (windowWidth,windowHeight);
+  fill ("lightblue");
+  strokeWeight (1);
+
+}
+
+function draw() {
+
+		background ("lightblue");
+  var num = 6;
+  var sideLen = windowWidth/num;
+ 
+  
+  for (var y=0; y < windowHeight; y = y+sideLen) {
+    for (var x=0; x < windowWidth; x=x+sideLen){
+image (gif, x, y, sideLen, sideLen);
+    
+          }
+}
+
+  
+}
+function windowResize (){
+  resizeCanvas (windowWidth, windowHeight);
+}
